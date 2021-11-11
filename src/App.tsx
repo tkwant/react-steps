403c577a861ed2e779e7637069b8040bc5cdd4c0
Tobias Kwant <t.kwant1@gmail.com>
2021-11-11 18:58:03 +0100

    expand demo

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,39 @@ import { Stepper, Step } from "./Stepper/Stepper";
 interface Props {}
 const App: React.FC<Props> = () => {
   const [curStep, setCurStep] = useState(0);
+  const [step2Data, setStep2Data] = useState("");
+  const renderContent = () => {
+    switch (curStep) {
+      case 0:
+        return <div className="flex justify-center mt-3">First Step</div>;
+      case 1:
+        return (
+          <div className="flex justify-center mt-3">
+            <p className="mr-2">My Validation: </p>
+            <input
+              className="border-4 border-red-300"
+              onChange={(e) => setStep2Data(e.currentTarget.value)}
+              value={step2Data}
+            />
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <>
       <Stepper curStep={curStep} setCurStep={setCurStep}>
         <Step label="Step 0 " />
         <Step label="Step 1" />
-        <Step>??</Step>
-        <Step />
+        <Step locked={step2Data === ""}>??</Step>
+        <Step locked={curStep < 2} />
         <Step locked={curStep < 3} />
         <Step locked={curStep < 4} />
         <Step locked={curStep < 5} />
       </Stepper>
+      {renderContent()}
     </>
   );
 };
